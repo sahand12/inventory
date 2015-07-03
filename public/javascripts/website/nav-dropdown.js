f796d04f9e93c7af2370d38cfb26c0e24ecdae91
Sahand Shokouhi <sahand.shok@gmail.com>
2015-07-04 02:16:11 +0430

    build the homepage up to domains carousel

--- public/javascripts/website/nav-dropdown.js
+++ public/javascripts/website/nav-dropdown.js
@@ -3,16 +3,18 @@ $(function() {
     var $dropdown = $('.dropdown'),
         $dropdownMenu = $('.dropdown-menu'),
         hoverColor = "rgb(46, 46, 46)",
-        normalColor = "white",
+        normalColor;
         $navbarToggle = $('.navbar-toggle');
 
     $dropdown.hover(
         function(event) {
             var flag = $navbarToggle.is(':hidden');
-            //$(this).find('a.menu-list-item').css({ color: hoverColor });
+            var $hoveredAnchor = $(this).find('a.menu-list-item');
+            normalColor = $hoveredAnchor.css('color');
+
+            $hoveredAnchor.css({ color: hoverColor });
             if (flag) {
                 $(this).find('.dropdown-menu').addClass('dropdown-list');
-                $(this).find('.dropdown-toggle').addClass('dropdown-border') ;
                 $(this).find('.dropdown-menu')
                     .css({ padding: 0 })
                     .show()
@@ -24,10 +26,9 @@ $(function() {
         },
         function (event) {
             var flag = $navbarToggle.is(':hidden');
-            //$(this).find('a.menuListItem').css({ color: normalColor });
+            $(this).find('a.menu-list-item').css({ color: normalColor });
             if (flag) {
                 $(this).find('.dropdown-menu').removeClass('dropdown-list');
-                $(this).find('.dropdown-toggle').removeClass('dropdown-border');
                 $(this).find('.dropdown-menu').hide();
             }
         }
