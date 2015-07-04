c3ac50d5a75fb0a8327bd7a743c4e67d4fdf8ea3
Sahand Shokouhi <sahand.shok@gmail.com>
2015-07-04 16:59:21 +0430

    make the domains slider

--- public/javascripts/website/homepage.js
+++ public/javascripts/website/homepage.js
@@ -24,4 +24,17 @@ $(function () {
      *      DOMAINS SECTION
      * ----------------------------------
      */
+    $('.slick-container').slick({
+        infinite: true,
+        //autoplay: true,
+        autoplaySpeed: 4000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.slick-prev-btn'),
+        nextArrow: $('.slick-next-btn')
+    });
+
+    $('.slick-prev').click(function () {
+        $('.slick-container').slickPrev();
+    })
 });
