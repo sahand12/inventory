11a82c0477df869e8b2cebb0afe2b060d260b70e
Sahand Shokouhi <sahand.shok@gmail.com>
2015-08-04 16:08:18 +0430

    profile and password sections of the settings profile now working only the others fields need to be implemented

--- controllers/cost/api/settingsApiHandler.js
+++ controllers/cost/api/settingsApiHandler.js
@@ -94,75 +94,7 @@ console.log(req.params);
             workflow.emit('findUserAndUpdate',fieldsToUpdate);
         });
 
-        workflow.on('validatePassword', function () {
-            console.log('err', req.body['profileCurrentPassword'], req.user.password);
-            Users.validatePassword(req.body['profileCurrentPassword'], req.user.password, function (err, isValid) {
-                if (err) {
-                    console.log(err);
-                    return res.send({
-                        success: false,
-                        postErrors: { error: 'database error' }
-                    });
-                }
-                if (!isValid) {
-                    return res.send({
-                        success: false,
-                        passwordError: { error: 'wrong password' }
-                    });
-                }
-                workflow.emit('validateNewPassword');
-            });
-        });
-
-        workflow.emit('validateNewPassword', function () {
-            req.checkBody('newPassword', 'Password should be at least 8 characters long.').len(8, 30);
-            req.checkBody('confirmPassword', 'Password don\'t match').equals(req.body['newPassword']);
-
-            var errors = req.validationErrors(true);
-            if (errors) {
-                return res.send({
-                    success: false,
-                    validationErrors: errors
-                });
-            }
-
-            workflow.emit('updatePassword', req.body['newPassword']);
-        });
-
-        workflow.on('updatePassword', function (newPass) {
-            Users.findOne({ _id: req.user._id }, function (err, user) {
-                if (err) {
-                    console.log(err);
-                    return res.send( {
-                        success: false,
-                        postErrors: { error: 'database error' }
-                    });
-                }
-                if (!user) {
-                    console.log('this is weird no user');
-                    return res.send({
-                        success: false,
-                        postErrors: { error: 'invalid request' }
-                    });
-                }
-
-                // set the new pass
-                user.password = newPass;
-                user.save(function (err) {
-                    if (err) {
-                        return res.send({
-                            success: false,
-                            postErrors: { error: 'database error' }
-                        });
-                    }
-                    return res.send({
-                        success: true
-                    });
-                });
-            });
-        });
-
-        workflow.on('findUserAndUpdate', function (fieldsToUpdate) {
+         workflow.on('findUserAndUpdate', function (fieldsToUpdate) {
             Users.findOne({ _id: req.user._id }, function (err, doc) {
                 if (err) {
                     return res.send({
@@ -193,6 +125,82 @@ console.log(req.params);
                     return res.send({
                         success: true,
                         data: doc
+                    });
+                });
+            });
+        });
+
+        workflow.on('validatePassword', function () {
+
+            Users.validatePassword(req.body['profileCurrentPassword'], req.user.password, function (err, isValid) {
+                if (err) {
+                    console.log('validatePassword error', err);
+                    return res.send({
+                        success: false,
+                        postErrors: { error: 'database error' }
+                    });
+                }
+                if (!isValid) {
+                    return res.send({
+                        success: false,
+                        validationErrors: {
+                            profileCurrentPassword: {
+                                msg: 'The provided password in not correct.',
+                                param: "profileCurrentPassword",
+                                value: ""
+                            }
+                        }
+                    });
+                }
+                console.log('validateNewPassword event fired');
+                workflow.emit('validateNewPassword');
+            });
+        });
+
+        workflow.on('validateNewPassword', function () {
+            req.checkBody('profileNewPassword', 'Password should be at least 8 characters long.').len(8, 30);
+            req.checkBody('profileNewConfirm', 'Password don\'t match').equals(req.body['profileNewPassword']);
+
+            var errors = req.validationErrors(true);
+            if (errors) {
+                return res.send({
+                    success: false,
+                    validationErrors: errors
+                });
+            }
+
+            workflow.emit('updatePassword', req.body['profileNewPassword']);
+        });
+
+        workflow.on('updatePassword', function (newPass) {
+            Users.findOne({ _id: req.user._id }, function (err, user) {
+                if (err) {
+                    console.log(err);
+                    return res.send( {
+                        success: false,
+                        postErrors: { error: 'database error' }
+                    });
+                }
+                if (!user) {
+                    console.log('this is weird no user');
+                    return res.send({
+                        success: false,
+                        postErrors: { error: 'invalid request' }
+                    });
+                }
+
+                // set the new pass
+                user.password = newPass;
+                user.save(function (err) {
+                    if (err) {
+                        console.log(err);
+                        return res.send({
+                            success: false,
+                            postErrors: { error: 'database error' }
+                        });
+                    }
+                    return res.send({
+                        success: true
                     });
                 });
             });
--- public/javascripts/cost/settings-page.js
+++ public/javascripts/cost/settings-page.js
@@ -9,6 +9,14 @@ $(function () {
     var $settingsTab = $('.settings-tab');
     var $settingsTabDestination = $('.settings-tab-destination');
     var $updateBtn = $settingsTabDestination.find('button');
+    var $formAjaxSpinner = $('.settings-form-ajax-spinner');
+    var $successMsg = $('.vgn-success-msg');
+    var $errorMsg = $('.vgn-error-msg');
+    var $closeBtn = $('.close');
+    var $settingsFormError = $('.settings-form-error-item');
+    var $settingsFormGroup = $('.settings-form').find('.form-group');
+
+    app.user = vgnUserProfile;
 
     /*
      * ---------------------
@@ -18,12 +26,31 @@ $(function () {
     $('#profileSettings').show();
 
     /*
+     * -------------------------
+     *      HELPER FUNCTIONS
+     * -------------------------
+     */
+    function extend (original, newObj) {
+        for (var field in newObj) {
+            if (newObj.hasOwnProperty(field)) {
+                if (typeof original[field] !== 'undefined') {
+                    original[field] = newObj[field];
+                }
+            }
+        }
+    }
+
+
+    /*
      * -----------------------
      *     CLICK HANDLER FOR TAB SELECTION
      * ----------------------
      */
     $settingsTab.on('click', function (e) {
         var $target = $(e.target);
+
+        // hide success or error messages
+        cleanForm();
 
         if ($target.hasClass('settings-tab-list')) {
             $target.siblings().removeClass('settings-tab-active');
@@ -35,6 +62,39 @@ $(function () {
             var $targetDest = $($target.attr('data-target'));
             $targetDest.fadeIn(300);
         }
+
+        populateSettingsForm ($targetDest);
+    });
+
+    function populateSettingsForm ($targetDest) {
+        var $inputs = $targetDest.find('input');
+
+        $inputs.each(function (i, elem) {
+            $(elem).val( app.user[elem.name] );
+        });
+    }
+
+    function cleanForm () {
+        // hide messages
+        $errorMsg.hide();
+        $successMsg.hide();
+
+        // first remove the class of .has-error
+        $settingsFormGroup.removeClass('has-error');
+
+        // clear the .settings-form-error-item
+        $settingsFormError.html("");
+    }
+
+    /*
+     * --------------------------------
+     *     CLOSE CLICK HANDLER FOR ALERTS
+     * --------------------------------
+     */
+    $closeBtn.on('click', function (e) {
+        e.preventDefault();
+
+        $(this).parent().hide();
     });
 
 
@@ -77,16 +137,56 @@ $(function () {
             data: data,
             beforeSend: updateUserAjaxInProgress
         }).done(function (response) {
-            updateUserAjaxEnded(response);
+            setTimeout(function () {
+                updateUserAjaxEnded(response, data);
+            }, 500);
         });
     }
 
     function updateUserAjaxInProgress () {
-
+        $settingsFormError.html("");
+        $settingsFormGroup.removeClass('has-error');
+        $formAjaxSpinner.show();
     }
 
-    function updateUserAjaxEnded (response) {
+    function updateUserAjaxEnded (response, data) {
+        $formAjaxSpinner.hide();
         console.log(response);
+        if (response.success) {
+            showSuccessMsg(response);
+
+            // update app.user
+            extend(app.user, data);
+        }
+        else {
+            showErrorMsg();
+            populateFormWithErrors(response);
+        }
+    }
+
+    function populateFormWithErrors (response) {
+        if (response.validationErrors) {
+            var errors = response.validationErrors;
+
+            for (var elemName in errors) {
+                if (errors.hasOwnProperty(elemName)) {
+                    var $inputDiv = $('#' + elemName).closest('.form-group');
+                    $inputDiv.addClass('has-error');
+                    $inputDiv.find('p').html("&bullet;&nbsp;&nbsp;" + errors[elemName].msg);
+                }
+            }
+        }
+    }
+
+
+    function showSuccessMsg (response) {
+        $errorMsg.hide();
+        $successMsg.show();
+    }
+
+    function showErrorMsg (response) {
+        $successMsg.hide();
+        $errorMsg.show();
     }
 
 
